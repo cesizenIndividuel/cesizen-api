ec20e0912f030a2a3d5c8385e1592cfab29a7adf
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-02-22 20:08:13 +0100

    refactor : reorganisation : séparation controller user et admin

---
+++ src/controllers/users.admin.controller.ts
@@ -0,0 +1,78 @@
+import { Request, Response } from "express";
+import { asyncHandler, parseOr400 } from "../utils/http";
+import { createUserSchema, userIdParamSchema, updateUserSchema } from "../validators/users.validators";
+import { usersService } from "../services/users.service";
+
+//----------------------------------//
+//          Creer un user           //
+//----------------------------------//
+export const createUser = asyncHandler (async (req: Request, res: Response) => {
+  // Validation du body
+  const body = parseOr400 (createUserSchema, req.body, res)
+  if (!body) return;
+
+  const result = await usersService.create(body);
+    if (!result.ok) {
+    return res.status(409).json({ error: result.error });
+  }
+  return res.status(201).json(result.user);
+})
+
+//----------------------------------//
+//          Liste des user          //
+//----------------------------------//
+export const getUsers = asyncHandler(async (req: Request, res: Response) => {
+  const users = await usersService.findAll();
+  return res.status(200).json(users);
+});
+
+//------------------------------------//
+//          Détail d'un user          //
+//------------------------------------//
+export const getUserById = asyncHandler(async (req: Request, res: Response) => {
+  const params = parseOr400 (userIdParamSchema, req.params, res)
+  if (!params) return;
+
+  const user = await usersService.findById(params.id);
+    if (!user){
+    return res.status(404).json({ error: "USER_NOT_FOUND" });
+  }
+  return res.status(200).json(user);
+});
+
+//-------------------------------------//
+//            MAJ d'un user            //
+//-------------------------------------//
+export const updateUser = asyncHandler (async (req: Request, res: Response) => {
+  // Valider id
+  const params = parseOr400 (userIdParamSchema, req.params, res)
+  if (!params) return;
+
+  //Valider Body
+  const body = parseOr400 (updateUserSchema, req.body, res)
+  if (!body) return;
+
+  const result = await usersService.updateById(params.id, body);
+  if (!result.ok) {
+    if (result.error === "EMAIL_ALREADY_USED") {
+      return res.status(409).json({ error: result.error });
+    }
+    return res.status(404).json({ error: result.error });
+  }
+  return res.status(200).json(result.user);
+})
+
+//-------------------------------------//
+//          Supprimer un user          //
+//-------------------------------------//
+//ADMIN
+export const deleteUser = asyncHandler(async (req: Request, res: Response) => {
+  const params = parseOr400 (userIdParamSchema, req.params, res)
+  if (!params) return;
+
+  const result = await usersService.deleteById(params.id);
+  if (!result.ok){
+      return res.status(404).json({ error: "USER_NOT_FOUND" });
+  } 
+  return res.status(204).send();
+})
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,23 +1,24 @@
 import { Router } from "express";
 import { uploadAvatar } from "../middlewares/upload.middleware";
-import * as usersController from "../controllers/users.controller";
 import { requireAuth } from "../middlewares/auth.middleware";
 import { requireRole } from "../middlewares/requireRole.middleware";
+import * as me from "../controllers/users.me.controller";
+import * as admin from "../controllers/users.admin.controller";
 
 export const usersRoutes = Router();
 
 usersRoutes.use(requireAuth);
 
 // ========== USER ==========//
-usersRoutes.get("/me", usersController.getMe);
-usersRoutes.patch("/me", usersController.updateMe);
-usersRoutes.patch("/me/password", usersController.updateMyPassword);
-usersRoutes.post("/me/avatar", uploadAvatar, usersController.updateMyAvatar);
-usersRoutes.delete("/me", usersController.deleteMe);
+usersRoutes.get("/me", me.getMe);
+usersRoutes.patch("/me", me.updateMe);
+usersRoutes.patch("/me/password", me.updateMyPassword);
+usersRoutes.post("/me/avatar", uploadAvatar, me.updateMyAvatar);
+usersRoutes.delete("/me", me.deleteMe);
 
 // ========== ADMIN ==========//
-usersRoutes.post("/", requireRole("ADMIN"), usersController.createUser);
-usersRoutes.get("/", requireRole("ADMIN"), usersController.getUsers);
-usersRoutes.get("/:id", requireRole("ADMIN"), usersController.getUserById);
-usersRoutes.patch("/:id", requireRole("ADMIN"), usersController.updateUser);
-usersRoutes.delete("/:id", requireRole("ADMIN"), usersController.deleteUser);
+usersRoutes.post("/", requireRole("ADMIN"), admin.createUser);
+usersRoutes.get("/", requireRole("ADMIN"), admin.getUsers);
+usersRoutes.get("/:id", requireRole("ADMIN"), admin.getUserById);
+usersRoutes.patch("/:id", requireRole("ADMIN"), admin.updateUser);
+usersRoutes.delete("/:id", requireRole("ADMIN"), admin.deleteUser);
