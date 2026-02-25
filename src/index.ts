bf8280c1c1bf391dd645c098e85785e59832bd1c
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-02-25 18:32:05 +0100

    feat : install cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import { router } from "./routes";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import path from "path";
 import express from "express";
+import cookieParser from "cookie-parser";
 
 app.use("/api", router);
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 app.use(errorMiddleware);
+app.use(cookieParser());
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 
