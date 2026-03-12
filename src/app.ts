ff001313d76f4815896a4637dd54b83ac3ee7227
LERMECHAIN <laura.ledoux@viacesi.fr>
2026-03-12 09:42:32 +0100

    feat : swagger pour authentification

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@
 
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import { swaggerSpec } from "./config/swagger";
 
 export const app = express();
 
 app.use(cors()); 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(express.json());
